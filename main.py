824b76c52ba99946bc4202d8ca4a7b201a21eadb
Bogdan Mazur <zochit21@gmail.com>
2024-01-22 12:24:26 +0200

    added more items

--- main.py
+++ main.py
@@ -3,22 +3,78 @@ import random
 
 bot = commands.Bot(command_prefix='!')
 
+weapons = [
+    # Штурмові гвинтівки
+    "M16A4",
+    "M416",
+    "SCAR-L",
+    "G36C",
+    "QBZ95",
+    "AK-47",
+    "Honey Badger",
+    "AUG",
+    # Марксманські гвинтівки
+    "SKS",
+    "Mk14 EBR",
+    "SLR",
+    "VSS",
+    "Mini14",
+    # Снайперські гвинтівки
+    "M24",
+    "AWM",
+    "Kar98K",
+    # Пістолети-кулемети
+    "Vector",
+    "UMP9",
+    "MP5K",
+    "PP-19 Bizon",
+    "Tommy Gun",
+    # Пістолети
+    "P92",
+    "P1911",
+    "R1895",
+    "Deagle",
+    "Glock 18",
+    # Озброєння ближнього бою
+    "Sawed-off",
+    "Double Barrel",
+    "Machete",
+    "Pan",
+    # Метальна зброя
+    "Crossbow",
+    "Flare Gun",
+    # Інше
+    "Throwables",
+]
 
-def generate_weapons():
-    weapons = [
-        "M416", "AKM", "Kar98k", "UMP45", "SCAR-L",
-        "Vector", "SKS", "Mini14", "S12K", "AWM"
-    ]
-    return weapons
+swap_items = [
+    "Шолом 1",
+    "Шолом 2",
+    "Шолом 3",
+    "Жилет",
+    "Жилет 1",
+    "Жилет 2",
+    "Жилет 3",
+    # Медичні приналежності
+    "Пакет першої допомоги",
+    "Медичний набір",
+    "Таблетки від болю",
+    "Енергетичний напій",
+    # Інше
+    "Димна граната",
+    "Молотов коктейль",
+    "С4",
+    "Граната з удушаючим газом",
+    "Коробка з повітрям",
+]
 
 
 def generate_inventory():
     inventory = {}
-    weapons = generate_weapons()
 
     inventory["weapon_1"] = random.choice(weapons)
     inventory["weapon_2"] = random.choice(weapons)
-    inventory["swap_item"] = random.choice(["затиск", "пристрій", "голограма"])
+    inventory["swap_item"] = random.choice(swap_items)
     inventory["swap_item_quantity"] = random.randint(0, 10)
 
     return inventory
@@ -47,7 +103,7 @@ async def generate_inventory_command(ctx, *players):
 async def change_weapon_command(ctx, player, slot):
     inventory_player = generate_inventory()
     current_weapon = inventory_player[f'weapon_{slot}']
-    new_weapon = random.choice(generate_weapons())
+    new_weapon = random.choice(weapons)
     inventory_player[f'weapon_{slot}'] = new_weapon
     inventory_message = (
         f"\nІм'я гравця: {player}\n"
